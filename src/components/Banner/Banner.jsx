d2bba1f40daa71aeef222dc99be9e104a0ee7213
arnab4477 <arnabsahareal@gmail.com>
2023-02-01 18:53:04 +0530

    Removed the horse image from the banner Added hover effects to header-icons

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,9 @@
 import './Banner.css';
-import Horse from './Horse.png';
 
 const Banner = () => {
   return (
     <div className="banner">
       <h1 className="banner-h1">Welcome to CheeseJS</h1>
-      <img className="horse" src={Horse} alt="Your" />
     </div>
   );
 };
